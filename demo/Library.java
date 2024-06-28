d88d7265a5fdc9618dc587c278f816fb8576bbc4
guruprasadcd <guruprasadcd@gmail.com>
2024-06-28 13:58:33 +0530

    Day 1

---
+++ demo/Library.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Library {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		{
+			System.out.println("addBook(String bookTitle)");
+		}
+		
+
+	}
+
+}
